69f0ce530e4b2f3d759e959c49c00d51b29fd99f
Kelly <lykelly2014@icloud.com>
2020-09-26 22:25:19 -0400

    search page working

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
 from flask import Flask, request, render_template, redirect, url_for
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import tweepy
+import os
+import requests
+import json
 
 app = Flask(__name__)
 analyzer = SentimentIntensityAnalyzer()
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(os.environ.get("CONSUMER_KEY"), os.environ.get("CONSUMER_SECRET"))
+auth.set_access_token(os.environ.get("ACCESS_TOKEN"), os.environ.get("ACCESS_TOKEN_SECRET"))
+base_url = "https://publish.twitter.com/oembed"
+
+# Create API object
+api = tweepy.API(auth, wait_on_rate_limit_notify=True)
+
+my_tweet = ""
+
+try:
+    api.verify_credentials()
+    print("Authentication OK")
+except:
+    print("Error during authentication")
 
 @app.route('/')
 def my_form():
@@ -11,13 +30,16 @@ def my_form():
 
 @app.route('/input', methods = ['POST'])
 def analyze():
+    global my_tweet
     my_tweet = request.form['my-tweet']
     score = analyzer.polarity_scores(my_tweet)
     sentiment_message = ""
+    sentiment_message2 = ""
     polarity_text = ""
 
     if score['compound'] < 0:
-        sentiment_message = "Are you sure you would like to tweet this? Harmful language may be hurtful to users of the platform. Please consider tweeting something else."
+        sentiment_message = "Are you sure you would like to tweet this? Harmful language may be hurtful to users of the platform."
+        sentiment_message2 = "Let's make sure we have #HealthyConversations on Twitter. Please consider tweeting something else."
         polarity_text = "Negative"
     elif score['compound'] == 0:
         sentiment_message = "Your tweet should be good to go!"
@@ -26,11 +48,65 @@ def analyze():
         sentiment_message = "Your tweet should be good to go. Thanks for spreading some positivity!"
         polarity_text = "Positive"
 
-    return render_template('tweet.html', score=score['compound'], my_tweet=my_tweet, polarity_text=polarity_text, sentiment_message=sentiment_message)
+    html_string = '<blockquote class="twitter-tweet"><p lang="en" dir="ltr">On the blog, the latest from Rick on tour info, adaptations and more! <a href="https://t.co/vsYgjAzp27">https://t.co/vsYgjAzp27</a> (artwork by Orpheus Collar) <a href="https://t.co/4j9Ccvgbx4">pic.twitter.com/4j9Ccvgbx4</a></p>&mdash; Rick Riordan (@rickriordan) <a href="https://twitter.com/rickriordan/status/1309896857930338304?ref_src=twsrc%5Etfw">September 26, 2020</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>'
+
+    return render_template('tweet-positive-neutral.html', score=score['compound'], my_tweet=my_tweet, polarity_text=polarity_text, sentiment_message=sentiment_message, sentiment_message2=sentiment_message2, html_string=html_string)
+
 
 @app.route('/tweet', methods = ['POST'])
 def tweet_it():
-    return render_template('timeline.html')
+    api.update_status(my_tweet)
+    return render_template('index.html')
+
+
+@app.route('/search', methods = ['POST'])
+def search_it():
+    search_string = request.form['search-string']
+    counter = 0
+    blocked = 0
+    html_string1 = ''
+    html_string2 = ''
+    html_string3 = ''
+    html_string4 = ''
+    html_string5 = ''
+    html_string6 = ''
+    html_string7 = ''
+    html_string8 = ''
+    html_string9 = ''
+    html_string10 = ''
+
+    for tweet in tweepy.Cursor(api.search, q=search_string, tweet_mode="extended").items(5):
+        url = f"https://twitter.com/{tweet.user.screen_name}/status/{tweet.id}"
+        payload = {'url': url}
+        response = requests.get(base_url, params=payload)
+        data = json.loads(response.text)
+
+        score = analyzer.polarity_scores(tweet.full_text)
+        if score['compound'] < 0:
+            blocked += 1
+            continue
+
+        counter += 1
+
+        if counter == 1: html_string1 = data['html']
+        elif counter == 2: html_string2 = data['html']
+        elif counter == 3: html_string3 = data['html']
+        elif counter == 4: html_string4 = data['html']
+        elif counter == 5: html_string5 = data['html']
+        elif counter == 6: html_string6 = data['html']
+        elif counter == 7: html_string7 = data['html']
+        elif counter == 8: html_string8 = data['html']
+        elif counter == 9: html_string9 = data['html']
+        elif counter == 10: html_string10 = data['html']
+
+    print(blocked)
+    search_message = 'Search results for: ' + search_string
+    return render_template('search.html', search_message=search_message, html_string1=html_string1, html_string2=html_string2, html_string3=html_string3, html_string4=html_string4, html_string5=html_string5, html_string6=html_string6, html_string7=html_string7, html_string8=html_string8, html_string9=html_string9, html_string10=html_string10)
+
+
+@app.route('/search_results', methods = ['POST'])
+def find_it():
+    return render_template('search.html')
 
 
 if __name__ == "__main__":
